3c3366e52e2aecec226b5109374f6d417d69fb15
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-04-14 14:32:00 +0100

    finished MVP queries

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,1 +1,22 @@
-SELECT * FROM animals;
+-- MVP
+-- query: find names of the animals in a given enclosure
+
+SELECT animals.name FROM enclosures
+INNER JOIN animals
+ON enclosures.id = animals.enclosure_id
+WHERE enclosures.id = 4;
+
+-- query: find names of the staff working in a given enclosure
+SELECT employees.name FROM employees
+INNER JOIN assignments
+ON employees.id = assignments.employee_id
+INNER JOIN enclosures
+ON enclosures.id = assignments.enclosure_id
+WHERE enclosures.id = 4;
+
+
+-- Extensions
+-- query:
+
+
+
